b6ffbf3cb19f7a1e698757bd5bc0a011cb5ef37f
keshara05 <kesharar99@gmail.com>
2026-03-21 09:43:27 +0530

    fix(vercel): route to correct backend URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://eduflexback.vercel.app' : 'http://localhost:5000';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend-eta-five-32.vercel.app' : 'http://localhost:5000';
 
 export default API_BASE_URL;
